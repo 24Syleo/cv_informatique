b81464f108c88bf0e95c7226332ec576fec55d03
24Syleo <sylvain.beggiora@outlook.com>
2023-03-12 19:45:04 +0100

    cv et contact

--- views/home.php
+++ views/home.php
@@ -4,7 +4,27 @@ require '../vendor/autoload.php';
 require '../elements/header.php';
 ?>
 
-<h1>Page d'accueil</h1>
+<main>
+   <section id="coordonne">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-6">
+                <address>
+                    <h6>Me Contacter</h6>
+                    <i class="bi bi-house my-3"> Chemin de la Gravière 31, 1225 Chêne-Bourg</i><br>
+                    <i class="bi bi-phone"><a href="tel:+41798745493" class="my-3"> 079 874 54 93</a></i><br>
+                    <i class="bi bi-envelope-at"><a href="mailto:sylvain.beggiora@outlook.com" class="my-3"> Mon email</a></i><br>
+                    <i class="bi bi-github"><a href="https://github.com/24Syleo"> Github</a></i><br>
+                    <i class="bi bi-linkedin"><a href="https://www.linkedin.com/in/sylvain-beggiora-151b258b/" class="my-3"> Linkedin</a></i>
+                </address>
+            </div>
+            <div class="col-6">
+
+            </div>
+        </div>
+    </div>
+   </section> 
+</main>
 
 
 <?php require '../elements/footer.php'; ?>
