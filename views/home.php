9071252fd8590c5ccde3519a426e6700a16c7db9
24Syleo <sylvain.beggiora@outlook.com>
2023-03-12 20:33:24 +0100

    modification image

--- views/home.php
+++ views/home.php
@@ -5,32 +5,29 @@ require '../elements/header.php';
 ?>
 
 <main>
-   <section id="coordonne">
-    <div class="container-fluid mt-2 text-center">
-        <div class="row align-items-center">
-            <div class="col-md-4">
-                <address class="text-start">
+    <section id="coordonne" class="bg-body-tertiary p-2">
+        <div class="d-md-flex flex-row justify-content-center align-items-center">
+            <div class="p-2">
+                <address class="">
                     <h6>Me Contacter</h6>
                     <i class="bi bi-house my-3"> Chemin de la Gravière 31, 1225 Chêne-Bourg</i><br>
                     <i class="bi bi-phone"><a href="tel:+41798745493" class="my-3"> 079 874 54 93</a></i><br>
-                    <i class="bi bi-envelope-at"><a href="mailto:sylvain.beggiora@outlook.com" class="my-3"> Mon email</a></i><br>
+                    <i class="bi bi-envelope-at"><a href="mailto:sylvain.beggiora@outlook.com" class="my-3"> Mon
+                            email</a></i><br>
                     <i class="bi bi-github"><a href="https://github.com/24Syleo"> Github</a></i><br>
                     <i class="bi bi-linkedin"><a href="https://www.linkedin.com/in/sylvain-beggiora-151b258b/" class="my-3"> Linkedin</a></i>
                 </address>
             </div>
-            <div class="col-4 d-none d-md-block">
+            <div class="p-2">
                 <div class="">
                     <a href="./pics/sylvain_beggiora_informatique.pdf">CV en format PDF</a>
                 </div>
             </div>
-            <div class="col-md-4">
-                <div>
-                    <img src="./pics/toonme_sylvain.jpg" alt="" class="img-fluid w-25 img-thumbnails rounded">
-                </div>
+            <div class="p-2 me-0">
+                <img src="./pics/sylvain.jpg" alt="" class="img-fluid w-100">
             </div>
         </div>
-    </div>
-   </section> 
+    </section>
 </main>
 
 
