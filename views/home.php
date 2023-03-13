15f6d9596cd1bef8e7ab942ab05442d4a6c8ee2f
24Syleo <sylvain.beggiora@outlook.com>
2023-03-13 19:08:42 +0100

    delete collapse competences

--- views/home.php
+++ views/home.php
@@ -28,33 +28,7 @@ require '../elements/header.php';
         </div>
     </section>
     <section id="competences" class="bg-body-secondary p-2">
-        <div class="container-fluid text-center">
-            <div class="row">
-                <div class="col-6">
-                    <button class="pdf-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExamplelangages" aria-expanded="false" aria-controls="collapseWidthExample">
-                        Langages informatiques
-                    </button>
-                    <div class="collapse collapse-horizontal" id="collapseWidthExamplelangages">
-                        <div class="card card-body text-center" style="width: 200px;">
-                            HTML / CSS
-                            JAVASCRIPT
-                            PHP
-                            MySql
-                        </div>
-                    </div>
-                </div>
-                <div class="col-6">
-                    <button class="pdf-link" type="button" data-bs-toggle="collapse" data-bs-target="#collapseWidthExample" aria-expanded="false" aria-controls="collapseWidthExample">
-                        Compétences
-                    </button>
-                    <div class="collapse collapse-horizontal" id="collapseWidthExample">
-                        <div class="card card-body" style="width: 300px;">
-                            This is some placeholder content for a horizontal collapse. It's hidden by default and shown when triggered.
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
+        
     </section>
 </main>
 
