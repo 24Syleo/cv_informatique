8bed3591ea4c5627bc424a38720952354653d7fe
24Syleo <sylvain.beggiora@outlook.com>
2023-05-30 18:41:05 +0200

    soft model et controller

--- controller.php
+++ controller.php
@@ -8,6 +8,7 @@ header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers
 // Récupération des Models
 require_once __DIR__ . "/model/Diplomes.php";
 require_once __DIR__ . "/model/Competences.php";
+require_once __DIR__ . "/model/Soft.php";
 
 // Récupération des constantes d'accès pour la base de données
 require_once "./config.php";
@@ -19,8 +20,10 @@ $uri = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 //Choisir le controller a appelé en fonction du chemin
 if (preg_match('#^/cv_informatique/diplomes#', $uri)) {
     $res = manageDiplomes();
-} else {
+} elseif(preg_match('#^/cv_informatique/competences#', $uri)) {
     $res = manageCompetences();
+} else{
+    $res = manageSofts();
 }
 
 echo (json_encode($res));
@@ -102,6 +105,46 @@ function manageCompetences()
                 }
                 $competences = $comp->searchCompetences($queryLimit, $queryOffset);
                 return $competences;
+            }
+    }
+}
+
+function manageSofts()
+{
+
+    //initialiser un objet competences
+    $soft = new Soft();
+
+    //method pour la base de données
+    $method =  $_SERVER['REQUEST_METHOD'];
+
+    //RéCUPéRER L'ID dans l'url
+    parse_str($_SERVER['QUERY_STRING'], $query);
+    $id = isset($query['id']) ? $query['id'] : '';
+
+    //récupérer les pages de diplomes
+    $queryPerPage = isset($query['per_page']) ? $query['per_page'] : '';
+    $queryPage = isset($query['page']) ? $query['page'] : '';
+
+    switch ($method) {
+        case 'GET':
+            if ($id) {
+                $soft = $soft->readSoft($id);
+                return $soft;
+            } else {
+                if ($queryPerPage) {
+                    $queryLimit = $queryPerPage;
+                    if ($queryPage) {
+                        $queryOffset = $queryPage;
+                    } else {
+                        $queryOffset = 0;
+                    }
+                } else {
+                    $queryLimit = 50;
+                    $queryOffset = 0;
+                }
+                $soft = $soft->searchSofts($queryLimit, $queryOffset);
+                return $soft;
             }
     }
 }
---
+++ model/Soft.php
@@ -0,0 +1,58 @@
+<?php
+require_once("./config.php");
+
+class Soft
+{
+    private $connection;
+
+    /**
+     * Constructor - Connect to the database.
+     */
+    public function __construct()
+    {
+        try {
+            // Connect to the database.
+            $this->connection = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_DATABASE_NAME, DB_USERNAME, DB_PASSWORD);
+        } catch (PDOException $e) {
+            // Send an error because it could not connect to the database.
+            throw new Exception($e->getMessage());
+        }
+    }
+
+    /**
+     * searchSoftss
+     *
+     * @return un tableau de competences douces
+     */
+    public function searchSofts($limit, $offset)
+    {
+        try {
+            $stmt = $this->connection->prepare("SELECT * FROM soft-skills
+                                            LIMIT $limit
+                                            OFFSET $offset");
+            $stmt->execute();
+            $competences = $stmt->fetchAll(PDO::FETCH_OBJ);
+            return $competences;
+        } catch (Exception $e) {
+            throw $e;
+        };
+    }
+
+    /**
+     * readSoft
+     *
+     * @param  mixed $id
+     * @return un Soft
+     */
+    public function readSoft($id)
+    {
+        try {
+            $stmt = $this->connection->prepare("SELECT * FROM soft-skills WHERE id = :id");
+            $stmt->execute(array(':id' => $id));
+            $competence = $stmt->fetch(PDO::FETCH_OBJ);
+            return $competence;
+        } catch (Exception $e) {
+            throw $e;
+        };
+    }
+}
